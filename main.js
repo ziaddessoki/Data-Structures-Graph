9ba1fc143d6d92bac5108a294ad8a8e432a4c008
Zee(Ziad) Eldesouky <ziad.dessoki@yahoo.com>
2020-08-12 20:06:41 +0000

    addVertex

--- main.js
+++ main.js
@@ -5,8 +5,12 @@ class Graph {
     }; 
   } 
   addVertex(node)  { 
+    this.adjacentList[node]= [];
+    this.numberOfNodes++;
   } 
   addEdge(node1, node2) { 
+    this.adjacentList[node1].push(node2)
+    this.adjacentList[node2].push(node2)
     //undirected Graph 
   } 
   showConnections() { 
